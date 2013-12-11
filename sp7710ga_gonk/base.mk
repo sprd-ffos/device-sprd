e10583051743f347bd8d7250bf64dfc5a354ac0e
james.zhang <james.zhang@spreadtrum.com>
2013-12-11 15:14:43 +0800

    bug#0 sync code from sprd-ffos

--- sp7710ga_gonk/base.mk
+++ sp7710ga_gonk/base.mk
@@ -10,10 +10,12 @@ ENABLE_LIBRECOVERY := true
 PRODUCT_PROPERTY_OVERRIDES := \
 	ro.moz.omx.hw.max_width=720 \
 	ro.moz.omx.hw.max_height=576 \
-	ro.moz.ril.query_icc_count=true
+	ro.moz.ril.query_icc_count=true \
+	ro.moz.mute.call.to_ril=true
 
 # original apps copied from generic_no_telephony.mk
 PRODUCT_PACKAGES := \
+	librecovery \
 	DeskClock \
 	Bluetooth \
 	Calculator \
